35bd794f29e000f01aa2e4e7046e8ecde54880a3
Jayanta Debnath <Jayanta.Dn@gmail.com>
2025-09-10 17:51:03 +0530

    docker

--- native.sh
+++ native.sh
@@ -55,3 +55,19 @@ WantedBy=default.target
 EOF
 systemctl --user daemon-reload
 systemctl --user enable --now rclone-gdrive.service
+
+# docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc || true
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker || true
+sudo usermod -aG docker $USER
